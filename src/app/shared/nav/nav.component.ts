e5c9d567132ee5866eba758583f634bcb406bdbb
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-12-08 15:15:36 +0100

    añadimos el enrutamiento a los enlaces

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-nav',
   standalone: true,
   imports: [CommonModule,RouterModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
 })
-export class LoginComponent {
+export class NavComponent {
   userLoginOn:boolean=false;
 }
