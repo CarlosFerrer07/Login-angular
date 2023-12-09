38ed0dd8e68e7483188179c257fbef0365968853
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-12-09 11:01:32 +0100

    añadimos form con su validacion y modificamos estilos

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule,FormBuilder ,Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  userLoginOn:boolean=false;
+
+  constructor(private formBuilder: FormBuilder, private router:Router) {}
+
+  loginForm = this.formBuilder.group({
+    email : ['carlosferrermartinez11@gmail.com',[Validators.required,Validators.email]],
+    password : ['',Validators.required]
+  })
+
+  get email() {
+    return this.loginForm.controls.email;
+  }
+
+  get password() {
+    return this.loginForm.controls.password;
+  }
+
+  login () {
+    if (this.loginForm.valid) {
+      console.log('Llamar al login');
+      this.router.navigateByUrl('/inicio');
+      this.loginForm.reset();
+    }else {
+      this.loginForm.markAllAsTouched();
+      alert('Ingrese los datos correctamente')
+    }
+  }
+
 }
